42495e66cd055a8545c437ffc0966ccc678cc930
KellyPT <trankphuong@gmail.com>
2020-11-20 00:09:15 -0800

    fix CORS error

--- server/app.js
+++ server/app.js
@@ -12,6 +12,11 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   req.requestTime = new Date().toISOString();
   next();
 });
